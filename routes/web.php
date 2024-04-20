6d577c200e8101b76ecd8276d4d45751e78e573f
masa-png <adidas.org@icloud.com>
2024-04-20 12:36:33 +0900

    投稿一覧ページの作成

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/post', [PostController::class, 'show'])->name('posts.show');
